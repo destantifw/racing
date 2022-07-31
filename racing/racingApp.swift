ed2387d29acd582819799fc69fe6ff77b9c2d3e6
DESTANTI FATWAKHYUNI <destantifw8@gmail.com>
2022-07-31 15:56:12 +0700

    Initial Commit

---
+++ racing/racingApp.swift
@@ -0,0 +1,17 @@
+//
+//  racingApp.swift
+//  racing
+//
+//  Created by destanti on 31/07/22.
+//
+
+import SwiftUI
+
+@main
+struct racingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
